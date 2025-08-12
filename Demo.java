5a5082e4e3fed32c5088dcc16291e34e7de68c66
siva2024-27 <shivaun3ak@gmail.com>
2025-08-12 19:24:53 +0530

    Delete Demo.java

--- Demo.java
+++
@@ -1,9 +0,0 @@
-package demo;
-
-public class Demo {
-    public String greeting = "Welcome to the Java package demo!";
-
-    public void showDetails() {
-        System.out.println("This method is showing extra details from the Demo class.");
-    }
-}
