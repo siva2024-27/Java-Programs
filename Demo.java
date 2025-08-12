d6fdf4310215f72ab279d25fb7ff35683cb637c8
siva2024-27 <shivaun3ak@gmail.com>
2025-08-12 19:24:21 +0530

    Demo.java

---
+++ Demo.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Demo {
+    public String greeting = "Welcome to the Java package demo!";
+
+    public void showDetails() {
+        System.out.println("This method is showing extra details from the Demo class.");
+    }
+}
